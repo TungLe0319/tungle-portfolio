1beb8eff1cdade9e2ac509c37cad82fbd6064a73
daniel <daniel97.le@gmail.com>
2023-01-06 16:59:28 -0700

    1

--- tungle-portfolio.client/vite.config.js
+++ tungle-portfolio.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../tungle-portfolio/client',
+    outDir: 'dist',
     sourcemap: false
   },
   server: {
